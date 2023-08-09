77d0741f56640e4106a6a5e23c33d9a122b0605b
Francisco Paradella <franciscoleite.dev@protonmail.com>
2023-08-08 23:50:40 -0300

    Add some user validations

--- lib/money_transfer/user.ex
+++ lib/money_transfer/user.ex
@@ -18,6 +18,11 @@ defmodule MoneyTransfer.User do
     user
     |> cast(attrs, [:first_name, :last_name, :cpf])
     |> validate_required([:first_name, :last_name, :cpf])
+    |> unique_constraint(:cpf)
+    |> validate_format(:cpf, ~r/(\d{3})?(\d{3})?(\d{3})?(\d{2})/)
+    |> validate_length(:first_name, min: 3)
+    |> validate_length(:last_name, min: 3)
+    |> validate_length(:cpf, is: 14)
   end
 
   def insert_user(attrs) do 
