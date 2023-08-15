10d7211da7ac1b86cd9b9b137a2fbb01d4a2a3d1
Francisco Paradella <franciscoleite.dev@protonmail.com>
2023-08-15 18:39:42 -0300

    remove create_user from test.ex

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,12 +1,2 @@
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(MoneyTransfer.Repo, :manual)
-
-def create_user do 
-  params = %{
-    first_name: "Test",
-    last_name: "Test", 
-    cpf: "810.631.740-#{Enum.random(10..99)}"
-  }
-
-  User.insert_user(params)
-end
