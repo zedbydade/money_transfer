5a9321a4c8a86645b8ac89853ad5c543c3ed37a3
Francisco Paradella <franciscoleite.dev@protonmail.com>
2023-08-15 18:39:23 -0300

    feat: Add user_mock functions

---
+++ test/support/user_mock.ex
@@ -0,0 +1,22 @@
+defmodule UserMock do 
+  alias MoneyTransfer.User 
+
+  def create_user(%{"first_name" => _, "last_name" => _, "cpf" => _} = params) do 
+    User.insert_user(params)
+  end
+
+  def create_user(%{"first_name" => _, "last_name" => _} = params) do 
+    params = Map.put(params, :cpf, "810.631.740-#{Enum.random(10..99)}")
+
+    User.insert_user(params)
+  end
+
+  def create_user do 
+    params = %{ first_name: "Test",
+      last_name: "Test", 
+      cpf: "810.631.740-#{Enum.random(10..99)}"
+    }
+
+    User.insert_user(params)
+  end
+end
