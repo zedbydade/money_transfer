583899250550f2aed1764fd4adc25b61b8ad43e0
Francisco Paradella <franciscoleite.dev@protonmail.com>
2023-08-24 23:53:49 -0300

    Create transactins

--- priv/repo/migrations/20230810021209_create_transactions.exs
+++ priv/repo/migrations/20230810021209_create_transactions.exs
@@ -4,8 +4,8 @@ defmodule MoneyTransfer.Repo.Migrations.CreateTransactions do
   def change do
     create table(:transactions) do 
       add :amount, :string
-      add :sender_id, references(:users, type: :uuid)
-      add :receiver_id, references(:users, type: :uuid)
+      add :sender_id, references(:users, type: :binary_id)
+      add :receiver_id, references(:users, type: :binary_id)
 
       timestamps()
     end
