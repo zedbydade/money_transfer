61bee830ad532a40a58765c4a8ffeabc967aa59a
Francisco Paradella <franciscoleite.dev@protonmail.com>
2023-08-24 23:53:34 -0300

    Add transaction validation

--- lib/money_transfer/transaction.ex
+++ lib/money_transfer/transaction.ex
@@ -6,14 +6,17 @@ defmodule MoneyTransfer.Transaction do
 
   schema "transactions" do 
     field :amount, :string 
-    belongs_to :sender, User, foreign_key: :sender_id 
-    belongs_to :receiver, User, foreign_key: :receiver_id 
+    belongs_to :sender, User, foreign_key: :sender_id, type: :binary_id 
+    belongs_to :receiver, User, foreign_key: :receiver_id, type: :binary_id 
+
+    timestamps()
   end
 
   def changeset(transaction, attrs) do 
     transaction 
     |> cast(attrs, [:amount, :sender_id, :receiver_id])
     |> validate_required([:amount, :sender_id, :receiver_id])
+    |> validate_users([:sender_id, :receiver_id])
   end
 
   def insert_transaction(attrs) do 
@@ -21,4 +24,13 @@ defmodule MoneyTransfer.Transaction do
     |> changeset(attrs)
     |> MoneyTransfer.Repo.insert
   end
+
+  def validate_users(changeset, field, _options  \\ []) do 
+    validate_change(changeset, field, fn _field, values -> 
+      valid? = values[0] !== values[1]
+      
+      if valid?, do: [], else: [{field, "Sender and Receiver cannot be the same"}]
+    end)
+  end
+
 end
